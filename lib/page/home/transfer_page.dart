9a5ef1d5962480164fd0479fdf2b5b9312885d39
wisnuibroo <wisnuaja0999@gmai.com>
2024-12-28 10:03:11 +0700

    FIX: [WF] update-ui-transfer-page

--- lib/page/home/transfer_page.dart
+++ lib/page/home/transfer_page.dart
@@ -22,102 +22,109 @@ class TransferPage extends StatelessWidget {
           fontWeight: FontWeight.bold,
           color: Colors.black,
         ),
-        backgroundColor: const Color(0xFFF8F9FA),
-        elevation: 0,
+        backgroundColor: Color(0xFFF8F9FA),
       ),
       backgroundColor: const Color(0xFFF8F9FA),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Obx(() {
-                final balance = balanceController.balance.value;
-                return Container(
-                  decoration: BoxDecoration(
-                    gradient: const LinearGradient(
-                      colors: [Color(0xFF025A5F), Color(0xFF00B4D8)],
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Obx(() {
+              final balance = balanceController.balance.value;
+              return Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Color(0xFF025A5F), Color(0xFF00B4D8)],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ),
+                  borderRadius: BorderRadius.circular(16),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 8,
+                      offset: Offset(0, 4),
                     ),
-                    borderRadius: BorderRadius.circular(16),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black26,
-                        blurRadius: 6,
-                        offset: const Offset(0, 3),
-                      ),
-                    ],
-                  ),
-                  padding: const EdgeInsets.all(16),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const MyText(
-                        text: "Saldo Anda:    ",
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                      const SizedBox(height: 8),
-                      MyText(
-                        text: "Rp $balance",
-                        fontSize: 28,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ],
-                  ),
-                );
-              }),
-              const SizedBox(height: 24),
-              const MyText(
-                text: "Masukkan Nomor Tujuan",
-                fontSize: 15,
-                fontWeight: FontWeight.bold,
-                color: Colors.black87,
-              ),
-              const SizedBox(height: 8),
-              MyTextField(
-                controller: transferController.recipientController,
-                hintText: "",
-                prefixText: '+62 ',
-                fillColor: Colors.white,
-                borderRadius: 12.0,
-              ),
-              const SizedBox(height: 24),
-              const MyText(
-                text: "Nominal Transfer",
-                fontSize: 15,
-                fontWeight: FontWeight.bold,
-                color: Colors.black87,
-              ),
-              const SizedBox(height: 8),
-              MyTextField(
-                controller: transferController.amountController,
-                hintText: "",
-                prefixText: 'Rp ',
-                fillColor: Colors.white,
-                borderRadius: 12.0,
-              ),
-              const SizedBox(height: 24),
-              SizedBox(
-                child: MyButton(
-                  onPressed: () {
-                    transferController.handleTransfer(context);
-                  },
-                  textButton: "Konfirmasi Transfer",
-                  backgroundColor: const Color(0xFF025A5F),
-                  textColor: Colors.white,
-                  radius: 10,
-                  width: double.infinity,
-                  height: 50,
+                  ],
                 ),
+                padding: EdgeInsets.all(20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        MyText(
+                          text: "Saldo Anda",
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                        SizedBox(height: 8),
+                        MyText(
+                          text: "Rp $balance",
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                    Image.asset(
+                      'assets/image/wiscash2.png',
+                      width: 110,
+                      height: 45,
+                    ),
+                  ],
+                ),
+              );
+            }),
+            SizedBox(height: 32),
+            MyText(
+              text: "Masukkan Nomor Tujuan",
+              fontSize: 12,
+              fontWeight: FontWeight.normal,
+              color: Colors.grey,
+            ),
+            SizedBox(height: 12),
+            MyTextField(
+              controller: transferController.recipientController,
+              hintText: " ",
+              prefixText: '+62 ',
+              fillColor: Colors.white,
+              borderRadius: 12.0,
+            ),
+            SizedBox(height: 24),
+            MyText(
+              text: "Nominal Transfer",
+              fontSize: 12,
+              fontWeight: FontWeight.normal,
+              color: Colors.grey,
+            ),
+            SizedBox(height: 12),
+            MyTextField(
+              controller: transferController.amountController,
+              hintText: " ",
+              prefixText: 'Rp ',
+              fillColor: Colors.white,
+              borderRadius: 12.0,
+            ),
+            Spacer(),
+            Center(
+              child: MyButton(
+                onPressed: () {
+                  transferController.handleTransfer(context);
+                },
+                textButton: "Konfirmasi Transfer",
+                backgroundColor: Color(0xFF025A5F),
+                textColor: Colors.white,
+                radius: 20,
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: 40,
               ),
-              const SizedBox(height: 30),
-            ],
-          ),
+            ),
+            SizedBox(height: 16),
+          ],
         ),
       ),
     );
