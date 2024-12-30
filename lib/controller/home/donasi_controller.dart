207cb88fab90cf7477f75a6679200560286ded56
wisnuibroo <wisnuaja0999@gmai.com>
2024-12-30 10:42:09 +0700

    FIX: [WF] donasi-

--- lib/controller/home/donasi_controller.dart
+++ lib/controller/home/donasi_controller.dart
@@ -4,19 +4,99 @@ import 'package:wiscash_/controller/home/balance_controller.dart';
 import 'package:wiscash_/controller/home/history_controller.dart';
 
 class DonasiController extends GetxController {
+  
+  final List<Map<String, String>> donasiList = [
+    {
+      'imageUrl':
+          'https://upload.wikimedia.org/wikipedia/commons/e/e6/Logo_BAZNAS_RI-Hijau-01.png',
+      'title': 'BAZNAS'
+    },
+    {
+      'imageUrl':
+          'https://glints.com/id/lowongan/wp-content/uploads/2021/07/4.-lowongan-public-relation-atau-pr-benihbaik-com.png',
+      'title': 'BenihBaik'
+    },
+    {
+      'imageUrl':
+          'https://upload.wikimedia.org/wikipedia/commons/d/d9/Logo-web-dd-hijau.png',
+      'title': 'Dompet Dhuafa'
+    },
+    {
+      'imageUrl':
+          'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEghTfmcQUwlVtT_F8dOLiT1pxpUhEfWgGDPk5J7GvvJL7hOSkepcEY-1SeZdKS3c0Di5PU9JcuJSje2Cd_dv2IerVt9rpL_lGvWYuJEaAXUWf1xXU-mXlZ06QqdMvn8JpuBN9T38zORPq8q/s400/logo.png',
+      'title': 'Generasi Peduli Sesama'
+    },
+    {
+      'imageUrl':
+          'https://kontenesia.com/wp-content/uploads/2022/02/logo-kitabisa.png',
+      'title': 'Kitabisa'
+    },
+    {
+      'imageUrl':
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHXjfUB7sm7TkCQWogUlZv-vg5BB8_nnh-VA&s',
+      'title': 'LazisNU'
+    },
+    {
+      'imageUrl':
+          'https://rumah-yatim.org/asset/frontend/img/Logo-Rumah-Yatim-Alt.png',
+      'title': 'Rumah Yatim'
+    },
+    {
+      'imageUrl': 'https://upload.wikimedia.org/wikipedia/id/a/a0/LOGO_RZ.png',
+      'title': 'Rumah Zakat'
+    },
+    {
+      'imageUrl':
+          'https://tdasurabaya.org/wp-content/uploads/2019/12/logo-tdasurabaya.png',
+      'title': 'Tangan Di Atas'
+    },
+    {
+      'imageUrl': 'https://spgusherindonesia.com/img/clients/wecare.id.png',
+      'title': 'WeCare id'
+    },
+    {
+      'imageUrl':
+          'https://www.al-azhar.or.id/wp-content/uploads/2024/07/Logo-YPIA-500px.png',
+      'title': 'Yayasan Al-Azhar'
+    },
+    {
+      'imageUrl':
+          'https://images.glints.com/unsafe/glints-dashboard.oss-ap-southeast-1.aliyuncs.com/company-logo/5389365b949ce0a9adc2b75a06fa5442.png',
+      'title': 'Yayasan Cinta Anak Bangsa'
+    },
+    {
+      'imageUrl':
+          'https://amalsholeh-s3.imgix.net/user-media/ZFs3HrY7c5WUS8PTBrcxXDl4XM3oRKPy5txrjlIn.png',
+      'title': 'Yayasan Peduli Ummat'
+    },
+    {
+      'imageUrl':
+          'https://yayasansayapibu.or.id/wp-content/uploads/2021/03/Logo-YSI.png',
+      'title': 'Yayasan Sayap Ibu'
+    },
+    {
+      'imageUrl':
+          'https://zdn.nz/site/tanganpengharapan/images/tangan-pengharapan-footer-logo.png',
+      'title': 'Yayasan Tangan Harapan'
+    },
+    {
+      'imageUrl': 'https://m.zakatpedia.com/static/media/logo.31e06fae.png',
+      'title': 'Zakatpedia'
+    },
+  ];
 
   final List<String> nominalList = [
-      "25.000",
-      "50.000",
-      "70.000",
-      "100.000",
-      "150.000",
-      "250.000",
-      "350.000",
-      "500.000",
-      "1.000.000"
-    ];
-    
+    "25.000",
+    "50.000",
+    "70.000",
+    "100.000",
+    "150.000",
+    "250.000",
+    "350.000",
+    "500.000",
+    "1.000.000"
+  ];
+
   void handleDonasi(BuildContext context, int nominal) {
     final balanceController = Get.find<BalanceController>();
     final historyController = Get.find<HistoryController>();
--- lib/page/home/donasi_page.dart
+++ lib/page/home/donasi_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:wiscash_/controller/home/donasi_controller.dart';
 import 'package:wiscash_/page/home/donasi_detail_page.dart';
 import 'package:wiscash_/page/widget/my_item_list.dart';
 import 'package:wiscash_/page/widget/my_text.dart';
@@ -12,94 +13,16 @@ class DonasiPage extends StatefulWidget {
 }
 
 class _DonasiPageState extends State<DonasiPage> {
+  
   final TextEditingController searchController = TextEditingController();
-
-  final List<Map<String, String>> donasiList = [
-    {
-      'imageUrl':
-          'https://upload.wikimedia.org/wikipedia/commons/e/e6/Logo_BAZNAS_RI-Hijau-01.png',
-      'title': 'BAZNAS'
-    },
-    {
-      'imageUrl':
-          'https://glints.com/id/lowongan/wp-content/uploads/2021/07/4.-lowongan-public-relation-atau-pr-benihbaik-com.png',
-      'title': 'BenihBaik'
-    },
-    {
-      'imageUrl':
-          'https://upload.wikimedia.org/wikipedia/commons/d/d9/Logo-web-dd-hijau.png',
-      'title': 'Dompet Dhuafa'
-    },
-    {
-      'imageUrl':
-          'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEghTfmcQUwlVtT_F8dOLiT1pxpUhEfWgGDPk5J7GvvJL7hOSkepcEY-1SeZdKS3c0Di5PU9JcuJSje2Cd_dv2IerVt9rpL_lGvWYuJEaAXUWf1xXU-mXlZ06QqdMvn8JpuBN9T38zORPq8q/s400/logo.png',
-      'title': 'Generasi Peduli Sesama'
-    },
-    {
-      'imageUrl':
-          'https://kontenesia.com/wp-content/uploads/2022/02/logo-kitabisa.png',
-      'title': 'Kitabisa'
-    },
-    {
-      'imageUrl':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHXjfUB7sm7TkCQWogUlZv-vg5BB8_nnh-VA&s',
-      'title': 'LazisNU'
-    },
-    {
-      'imageUrl':
-          'https://rumah-yatim.org/asset/frontend/img/Logo-Rumah-Yatim-Alt.png',
-      'title': 'Rumah Yatim'
-    },
-    {
-      'imageUrl': 'https://upload.wikimedia.org/wikipedia/id/a/a0/LOGO_RZ.png',
-      'title': 'Rumah Zakat'
-    },
-    {
-      'imageUrl':
-          'https://tdasurabaya.org/wp-content/uploads/2019/12/logo-tdasurabaya.png',
-      'title': 'Tangan Di Atas'
-    },
-    {
-      'imageUrl': 'https://spgusherindonesia.com/img/clients/wecare.id.png',
-      'title': 'WeCare id'
-    },
-    {
-      'imageUrl':
-          'https://www.al-azhar.or.id/wp-content/uploads/2024/07/Logo-YPIA-500px.png',
-      'title': 'Yayasan Al-Azhar'
-    },
-    {
-      'imageUrl':
-          'https://images.glints.com/unsafe/glints-dashboard.oss-ap-southeast-1.aliyuncs.com/company-logo/5389365b949ce0a9adc2b75a06fa5442.png',
-      'title': 'Yayasan Cinta Anak Bangsa'
-    },
-    {
-      'imageUrl':
-          'https://amalsholeh-s3.imgix.net/user-media/ZFs3HrY7c5WUS8PTBrcxXDl4XM3oRKPy5txrjlIn.png',
-      'title': 'Yayasan Peduli Ummat'
-    },
-    {
-      'imageUrl':
-          'https://yayasansayapibu.or.id/wp-content/uploads/2021/03/Logo-YSI.png',
-      'title': 'Yayasan Sayap Ibu'
-    },
-    {
-      'imageUrl':
-          'https://zdn.nz/site/tanganpengharapan/images/tangan-pengharapan-footer-logo.png',
-      'title': 'Yayasan Tangan Harapan'
-    },
-    {
-      'imageUrl': 'https://m.zakatpedia.com/static/media/logo.31e06fae.png',
-      'title': 'Zakatpedia'
-    },
-  ];
+  final DonasiController donasiController = Get.put(DonasiController());
 
   List<Map<String, String>> filteredList = [];
 
   @override
   void initState() {
     super.initState();
-    filteredList = List.from(donasiList);
+    filteredList = List.from(donasiController.donasiList);
 
     searchController.addListener(_filterDonasiList);
   }
@@ -114,7 +37,7 @@ class _DonasiPageState extends State<DonasiPage> {
   void _filterDonasiList() {
     final query = searchController.text.toLowerCase();
     setState(() {
-      filteredList = donasiList.where((item) {
+      filteredList = donasiController.donasiList.where((item) {
         final title = item['title']!.toLowerCase();
         return title.contains(query);
       }).toList();
